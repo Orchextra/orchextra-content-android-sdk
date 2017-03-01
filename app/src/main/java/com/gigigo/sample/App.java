de30b5358a3c02fa91e0adcfc94d70ed342cadde
Jose Benito <jfbenitopa@gmail.com>
2017-03-01 17:57:29 +0100

    Upgrade version to try

--- app/src/main/java/com/gigigo/sample/App.java
+++ app/src/main/java/com/gigigo/sample/App.java
@@ -1,12 +1,16 @@
 package com.gigigo.sample;
 
 import android.app.Application;
+import com.gigigo.orchextra.ocm.Ocm;
+import com.gigigo.orchextra.ocm.OcmBuilder;
 
 public class App extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
 
-    
+    OcmBuilder ocmBuilder = new OcmBuilder(this).setNotificationActivityClass(MainActivity.class).setOrchextraCredentials("","");
+
+    Ocm.initialize(ocmBuilder);
   }
 }
