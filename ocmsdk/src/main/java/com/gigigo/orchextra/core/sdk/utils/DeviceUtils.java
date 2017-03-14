cf5bf59f599eb6df3f0eb5d4deb9c3bbd334c372
Jose Benito <jfbenitopa@gmail.com>
2017-03-14 14:37:55 +0100

    Width and height device depends on android sdk version

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/utils/DeviceUtils.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/utils/DeviceUtils.java
@@ -1,22 +1,45 @@
 package com.gigigo.orchextra.core.sdk.utils;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.WindowManager;
+import com.gigigo.ggglib.device.AndroidSdkVersion;
 
 public class DeviceUtils {
 
-  public static int calculateRealHeightDevice(Context context) {
-    DisplayMetrics metrics = new DisplayMetrics();
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1) public static int calculateRealHeightDevice(Context context) {
     WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-    wm.getDefaultDisplay().getRealMetrics(metrics);
-    return metrics.heightPixels;
+
+    DisplayMetrics metrics = new DisplayMetrics();
+    Display display = wm.getDefaultDisplay();
+
+    if (AndroidSdkVersion.hasJellyBean17()) {
+      display.getRealMetrics(metrics);
+      return metrics.heightPixels;
+    } else {
+      Point size = new Point();
+      display.getSize(size);
+      return size.y;
+    }
   }
 
-  public static int calculateRealWidthDevice(Context context) {
-    DisplayMetrics metrics = new DisplayMetrics();
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1) public static int calculateRealWidthDevice(Context context) {
     WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-    wm.getDefaultDisplay().getRealMetrics(metrics);
-    return metrics.widthPixels;
+
+    DisplayMetrics metrics = new DisplayMetrics();
+    Display display = wm.getDefaultDisplay();
+
+    if (AndroidSdkVersion.hasJellyBean17()) {
+      display.getRealMetrics(metrics);
+      return metrics.widthPixels;
+    } else {
+      Point size = new Point();
+      display.getSize(size);
+      return size.x;
+    }
   }
 }
