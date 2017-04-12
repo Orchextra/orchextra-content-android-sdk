cd618a3d7e167a26133e5dbf39a446d12fab757a
Jose Benito <jfbenitopa@gmail.com>
2017-04-12 17:16:55 +0200

    Doing callback when received error from Orchextra initialization

--- ocmsdk/src/main/java/com/gigigo/orchextra/ocm/callbacks/OcmCredentialCallback.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/ocm/callbacks/OcmCredentialCallback.java
@@ -7,4 +7,5 @@ package com.gigigo.orchextra.ocm.callbacks;
 // Intermediate ocm callback to return ox access token to app
 public interface OcmCredentialCallback {
   void onCredentialReceiver(String accessToken);
+  void onCredentailError(String code);
 }
