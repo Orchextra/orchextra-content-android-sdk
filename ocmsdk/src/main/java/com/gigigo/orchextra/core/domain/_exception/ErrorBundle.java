402d7778b33ead04453606d19cc5e25ea4002525
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-22 17:35:28 +0200

    ErrorBundles added to manage errors in domain layer

---
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/domain/_exception/ErrorBundle.java
@@ -0,0 +1,10 @@
+package com.gigigo.orchextra.core.domain._exception;
+
+/**
+ * Interface to represent a wrapper around an {@link Exception} to manage errors.
+ */
+public interface ErrorBundle {
+  Exception getException();
+
+  String getErrorMessage();
+}
