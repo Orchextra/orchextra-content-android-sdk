385acf0633d8d372061c72054f864858349ef7ba
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-22 17:33:25 +0200

    Repository interface added

---
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/domain/_repository/OcmRepository.java
@@ -0,0 +1,22 @@
+package com.gigigo.orchextra.core.domain._repository;
+
+import com.gigigo.orchextra.core.domain.entities.contentdata.ContentItem;
+import com.gigigo.orchextra.core.domain.entities.elementcache.ElementCache;
+import com.gigigo.orchextra.core.domain.entities.menus.MenuContentData;
+import io.reactivex.Observable;
+
+/**
+ * Created by francisco.hernandez on 22/5/17.
+ */
+
+public interface OcmRepository {
+  Observable<MenuContentData> getMenu(boolean useCache);
+
+  Observable<ElementCache> getCachedElement(String elementUrl);
+
+  Observable<ElementCache> getElementCacheBySection(String section);
+
+  Observable<String> getContentUrlBySection(String section);
+
+  Observable<ContentItem> getSectionContentById(String section);
+}
