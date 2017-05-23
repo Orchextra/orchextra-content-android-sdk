bf67dbeb2a970bc8a6653936a2f3febca40ec148
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-23 15:26:11 +0200

    DataStoreFactory added

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxRepository/rxDatasource/OcmDataStoreFactory.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxRepository/rxDatasource/OcmDataStoreFactory.java
@@ -1,8 +1,29 @@
 package com.gigigo.orchextra.core.data.rxRepository.rxDatasource;
 
+import orchextra.javax.inject.Inject;
+import orchextra.javax.inject.Named;
+import orchextra.javax.inject.Singleton;
+
 /**
  * Created by francisco.hernandez on 23/5/17.
  */
 
-public class OcmDataStoreFactory {
+@Singleton public class OcmDataStoreFactory {
+
+  private final OcmDataStore cloudDataStore;
+  private final OcmDataStore diskDataStore;
+
+  @Inject public OcmDataStoreFactory(@Named("cloud_ocm_datastore") OcmDataStore cloudDataStore,
+      @Named("cloud_ocm_datastore") OcmDataStore diskDataStore) {
+    this.cloudDataStore = cloudDataStore;
+    this.diskDataStore = diskDataStore;
+  }
+
+  public OcmDataStore getCloudDataStore() {
+    return cloudDataStore;
+  }
+
+  public OcmDataStore getDiskDataStore() {
+    return diskDataStore;
+  }
 }
