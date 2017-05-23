ce5690d3db035f9f5c3e4de64e5ba0ea9e446b81
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-23 09:28:55 +0200

    OcmDataStore added

---
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxRepository/rxDatasource/OcmDataStore.java
@@ -0,0 +1,29 @@
+package com.gigigo.orchextra.core.data.rxRepository.rxDatasource;
+
+import com.gigigo.orchextra.core.data.api.dto.content.ApiSectionContentDataResponse;
+import com.gigigo.orchextra.core.data.api.dto.elementcache.ApiElementDataResponse;
+import com.gigigo.orchextra.core.data.api.dto.menus.ApiMenuContentDataResponse;
+import io.reactivex.Observable;
+
+/**
+ * Interface that represents a data store from where data is retrieved.
+ */
+public interface UserDataStore {
+
+  /**
+   * Get an {@link Observable} which will emit a {@link ApiMenuContentDataResponse}.
+   */
+  Observable<ApiMenuContentDataResponse> getMenuEntity(boolean forceReload);
+  /**
+   * Get an {@link Observable} which will emit a {@link ApiSectionContentDataResponse}.
+   */
+  Observable<ApiSectionContentDataResponse> getSectionEntity(String elementUrl);
+  /**
+   * Get an {@link Observable} which will emit a {@link ApiSectionContentDataResponse}.
+   */
+  Observable<ApiSectionContentDataResponse> searchByText(String section);
+  /**
+   * Get an {@link Observable} which will emit a {@link ApiElementDataResponse}.
+   */
+  Observable<ApiElementDataResponse> getElementById(String section);
+}
