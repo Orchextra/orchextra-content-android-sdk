59cb2ceb1bcb9ed9a98e7f6d456a916743736334
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-23 16:29:10 +0200

    Menus implemented in OcmDataRepository

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxRepository/OcmDataRepository.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxRepository/OcmDataRepository.java
@@ -1,15 +1,50 @@
 package com.gigigo.orchextra.core.data.rxRepository;
 
+import com.gigigo.orchextra.core.data.api.mappers.menus.ApiMenuContentListResponseMapper;
+import com.gigigo.orchextra.core.data.rxRepository.rxDatasource.OcmDataStore;
+import com.gigigo.orchextra.core.data.rxRepository.rxDatasource.OcmDataStoreFactory;
 import com.gigigo.orchextra.core.domain.entities.contentdata.ContentItem;
 import com.gigigo.orchextra.core.domain.entities.elementcache.ElementCache;
 import com.gigigo.orchextra.core.domain.entities.menus.MenuContentData;
 import com.gigigo.orchextra.core.domain.rxRepository.OcmRepository;
 import io.reactivex.Observable;
+import orchextra.javax.inject.Inject;
+import orchextra.javax.inject.Singleton;
 
 /**
  * Created by francisco.hernandez on 23/5/17.
  */
 
-public class OcmDataRepository {
+@Singleton
+public class OcmDataRepository implements OcmRepository {
+  private final OcmDataStoreFactory ocmDataStoreFactory;
+  private final ApiMenuContentListResponseMapper apiMenuContentListResponseMapper;
 
+  @Inject
+  public OcmDataRepository(OcmDataStoreFactory ocmDataStoreFactory,
+      ApiMenuContentListResponseMapper apiMenuContentListResponseMapper) {
+    this.ocmDataStoreFactory = ocmDataStoreFactory;
+    this.apiMenuContentListResponseMapper = apiMenuContentListResponseMapper;
+  }
+
+  @Override public Observable<MenuContentData> getMenu(boolean forceReload) {
+    OcmDataStore ocmDataStore = ocmDataStoreFactory.getCloudDataStore();
+    return ocmDataStore.getMenuEntity().map(apiMenuContentListResponseMapper::externalClassToModel);
+  }
+
+  @Override public Observable<ElementCache> getCachedElement(String elementUrl) {
+    return null;
+  }
+
+  @Override public Observable<ElementCache> getElementCacheBySection(String section) {
+    return null;
+  }
+
+  @Override public Observable<String> getContentUrlBySection(String section) {
+    return null;
+  }
+
+  @Override public Observable<ContentItem> getSectionContentById(String section) {
+    return null;
+  }
 }
