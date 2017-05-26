657ee6bc17dcedc59f1acf40f7f4d0291cfd7372
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-05-26 12:45:21 +0200

    Uncomment methods

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/controller/OcmControllerImp.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/controller/OcmControllerImp.java
@@ -11,6 +11,7 @@ import com.gigigo.orchextra.core.domain.invocators.GridElementsInteractorInvocat
 import com.gigigo.orchextra.core.domain.invocators.MenuInteractorInvocator;
 import com.gigigo.orchextra.core.domain.rxInteractor.DefaultObserver;
 import com.gigigo.orchextra.core.domain.rxInteractor.GetMenus;
+import java.util.Map;
 
 public class OcmControllerImp implements OcmController {
 
@@ -57,111 +58,70 @@ public class OcmControllerImp implements OcmController {
       getMenusCallback.onGetMenusLoaded(menuContentData);
     }
   }
-  //
-  //@Override
-  //public ElementCache getElementCacheBySection(String section) {
-  //  MenuContentData savedMenuContentData = menuInteractorInvocator.getMenu(true);
-  //  if (savedMenuContentData == null || savedMenuContentData.getElementsCache() == null) {
-  //    return null;
-  //  }
-  //
-  //  return savedMenuContentData.getElementsCache().get(section);
-  //}
-  //
-  //@Override public String getContentUrlBySection(String section) {
-  //  ElementCache elementCache = getElementCacheBySection(section);
-  //
-  //  if (elementCache == null || elementCache.getRender() == null) {
-  //    return null;
-  //  }
-  //
-  //  return elementCache.getRender().getContentUrl();
-  //}
-  //
-  //@Override public ContentItem getSectionContentById(String section) {
-  //  return detailContentElementInteractorInvocator.getDetailSectionContentBySection(section);
-  //}
-  //
-  //@Override public void saveSectionContentData(String section, ContentData contentData) {
-  //  detailContentElementInteractorInvocator.saveDetailSectionContentBySection(section, contentData.getContent());
-  //
-  //  Map<String, ElementCache> elementsCache = contentData.getElementsCache();
-  //  if (elementsCache != null) {
-  //    for (String key : elementsCache.keySet()) {
-  //      gridElementsInteractorInvocator.saveElementById(key, elementsCache.get(key));
-  //    }
-  //  }
-  //}
-  //
-  //@Override public void clearCache() {
-  //  menuInteractorInvocator.clear();
-  //  detailContentElementInteractorInvocator.clear();
-  //  gridElementsInteractorInvocator.clear();
-  //}
-  //
-  //@Override public ElementCache getCachedElement(final String elementUrl) {
-  //  try {
-  //    ElementCache elementCache = gridElementsInteractorInvocator.getElementById(elementUrl);
-  //
-  //    if (elementCache == null) {
-  //      String slug = getSlug(elementUrl);
-  //      elementCache = gridElementsInteractorInvocator.getElementById(slug);
-  //    }
-  //
-  //    return elementCache;
-  //  } catch (Exception e) {
-  //    return null;
-  //  }
-  //}
-  //
-  //private String getSlug(String elementUrl) {
-  //
-  //  try {
-  //    return elementUrl.substring(elementUrl.lastIndexOf("/") + 1, elementUrl.length());
-  //  } catch (Exception ignored) {
-  //    return null;
-  //  }
-  //}
 
-
-
-
-  private final class UserListObserver extends DefaultObserver<MenuContentData> {
-
-    @Override public void onComplete() {
-
+  @Override
+  public ElementCache getElementCacheBySection(String section) {
+    MenuContentData savedMenuContentData = menuInteractorInvocator.getMenu(true);
+    if (savedMenuContentData == null || savedMenuContentData.getElementsCache() == null) {
+      return null;
     }
 
-    @Override public void onError(Throwable e) {
-      //presenter.this.showErrorMessage(new DefaultErrorBundle((Exception) e));
-    }
-
-    @Override public void onNext(MenuContentData menuContentData) {
-      Log.v("MenuContentData", "Elements: " + menuContentData.getMenuContentList().size());
-    }
-  }
-
-  @Override public ElementCache getCachedElement(String elementUrl) {
-    return null;
-  }
-
-  @Override public ElementCache getElementCacheBySection(String section) {
-    return null;
+    return savedMenuContentData.getElementsCache().get(section);
   }
 
   @Override public String getContentUrlBySection(String section) {
-    return null;
+    ElementCache elementCache = getElementCacheBySection(section);
+
+    if (elementCache == null || elementCache.getRender() == null) {
+      return null;
+    }
+
+    return elementCache.getRender().getContentUrl();
   }
 
   @Override public ContentItem getSectionContentById(String section) {
-    return null;
+    return detailContentElementInteractorInvocator.getDetailSectionContentBySection(section);
   }
 
   @Override public void saveSectionContentData(String section, ContentData contentData) {
+    detailContentElementInteractorInvocator.saveDetailSectionContentBySection(section, contentData.getContent());
 
+    Map<String, ElementCache> elementsCache = contentData.getElementsCache();
+    if (elementsCache != null) {
+      for (String key : elementsCache.keySet()) {
+        gridElementsInteractorInvocator.saveElementById(key, elementsCache.get(key));
+      }
+    }
   }
 
   @Override public void clearCache() {
-
+    menuInteractorInvocator.clear();
+    detailContentElementInteractorInvocator.clear();
+    gridElementsInteractorInvocator.clear();
   }
+
+  @Override public ElementCache getCachedElement(final String elementUrl) {
+    try {
+      ElementCache elementCache = gridElementsInteractorInvocator.getElementById(elementUrl);
+
+      if (elementCache == null) {
+        String slug = getSlug(elementUrl);
+        elementCache = gridElementsInteractorInvocator.getElementById(slug);
+      }
+
+      return elementCache;
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  private String getSlug(String elementUrl) {
+
+    try {
+      return elementUrl.substring(elementUrl.lastIndexOf("/") + 1, elementUrl.length());
+    } catch (Exception ignored) {
+      return null;
+    }
+  }
+
 }
