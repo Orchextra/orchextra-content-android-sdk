93f374502b29cc47b4a700616a1ec76555e540d8
Alberto Sainz Vegas <nuborisar@gmail.com>
2017-06-08 14:51:48 +0200

    update dependencies

--- app/src/main/java/com/gigigo/sample/App.java
+++ app/src/main/java/com/gigigo/sample/App.java
@@ -17,15 +17,15 @@ public class App extends MultiDexApplication {
   //public static String API_KEY = "a2966ba69f4ead1a4f1550bfda450e9fd07e6762";   //Asv project
   //public static String API_SECRET = "f79713d7e9b0fcd69fedfb94f471106cb85d8ca4";
 
-  public static String API_KEY = "8286702045adf5a3ad816f70ecb80e4c91fbb8de";    //Woah project
-  public static String API_SECRET = "eab37080130215ced60eb9d5ff729049749ec205";
+  //public static String API_KEY = "8286702045adf5a3ad816f70ecb80e4c91fbb8de";    //Woah project
+  //public static String API_SECRET = "eab37080130215ced60eb9d5ff729049749ec205";
   //
   //public static String API_KEY = "adfc8ba4340828a054bf061f692707a197af96cb";    //[UAT][CSE] - WOAH SITC project --- Staging
   //public static String API_SECRET = "677cf75a17aeec144ee402c281ad3a732d736a8a";
 
   //REPSOL RELEASE
-  //public static String API_KEY = "7bb9fa0f9b7a02846383fd6284d3c74b8155644c";    //[UAT][CSE] - DEMO CONTENT
-  //public static String API_SECRET = "3295dc8de90300e2977e6cec5b28b614fc644934";
+  public static String API_KEY = "7bb9fa0f9b7a02846383fd6284d3c74b8155644c";
+  public static String API_SECRET = "3295dc8de90300e2977e6cec5b28b614fc644934";
 
   private OnRequiredLoginCallback onDoRequiredLoginCallback = new OnRequiredLoginCallback() {
     @Override public void doRequiredLogin() {
