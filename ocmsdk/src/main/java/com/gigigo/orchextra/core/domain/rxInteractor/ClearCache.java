72ce5255b8d5fc1e47ce4dc74e4f58ef2e774178
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-06-14 15:30:24 +0200

    Clear Usedata added

---
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/domain/rxInteractor/ClearCache.java
@@ -0,0 +1,43 @@
+package com.gigigo.orchextra.core.domain.rxInteractor;
+
+import com.gigigo.orchextra.core.domain.entities.contentdata.ContentData;
+import com.gigigo.orchextra.core.domain.rxExecutor.PostExecutionThread;
+import com.gigigo.orchextra.core.domain.rxExecutor.ThreadExecutor;
+import com.gigigo.orchextra.core.domain.rxRepository.OcmRepository;
+import io.reactivex.Observable;
+import orchextra.javax.inject.Inject;
+
+/**
+ * This class is an implementation of {@link UseCase} that represents a use case for
+ * deleting stored data}.
+ */
+public class ClearCache extends UseCase<ContentData, ClearCache.Params> {
+
+  private final OcmRepository ocmRepository;
+
+  @Inject ClearCache(OcmRepository ocmRepository, ThreadExecutor threadExecutor,
+      PostExecutionThread postExecutionThread) {
+    super(threadExecutor, postExecutionThread);
+    this.ocmRepository = ocmRepository;
+  }
+
+  @Override Observable<ContentData> buildUseCaseObservable(Params params) {
+    //return this.ocmRepository.getSectionElements(params.forceReload, params.section);
+    return null;
+  }
+
+  public static final class Params {
+
+    private final boolean images;
+    private final boolean data;
+
+    private Params(boolean images, boolean data) {
+      this.images = images;
+      this.data = data;
+    }
+
+    public static Params create(boolean forceReload, boolean section) {
+      return new Params(forceReload, section);
+    }
+  }
+}
