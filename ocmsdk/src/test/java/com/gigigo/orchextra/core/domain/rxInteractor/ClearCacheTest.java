8e78534f26d5cf0106567531ced23ccd41f724b7
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-06-19 10:03:53 +0200

    ClearCache use case added

---
+++ ocmsdk/src/test/java/com/gigigo/orchextra/core/domain/rxInteractor/ClearCacheTest.java
@@ -0,0 +1,52 @@
+package com.gigigo.orchextra.core.domain.rxInteractor;
+
+import com.gigigo.orchextra.core.domain.rxExecutor.PostExecutionThread;
+import com.gigigo.orchextra.core.domain.rxExecutor.ThreadExecutor;
+import com.gigigo.orchextra.core.domain.rxRepository.OcmRepository;
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.TestScheduler;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+@RunWith(MockitoJUnitRunner.class) public class ClearCacheTest {
+
+  private static final boolean FORCE_RELOAD = false;
+
+  private GetMenus getMenus;
+
+  @Mock private OcmRepository mockOcmRepository;
+  @Mock private ThreadExecutor mockThreadExecutor;
+  @Mock private PostExecutionThread mockPostExecutionThread;
+
+  @Rule public ExpectedException expectedException = ExpectedException.none();
+
+  @Before public void setUp() {
+    getMenus = new GetMenus(mockOcmRepository, mockThreadExecutor, mockPostExecutionThread);
+  }
+
+  @Test public void testGetUserDetailsUseCaseObservableHappyCase() {
+    getMenus.buildUseCaseObservable(GetMenus.Params.forForceReload(FORCE_RELOAD));
+
+    verify(mockOcmRepository).getMenu(FORCE_RELOAD);
+    verifyNoMoreInteractions(mockOcmRepository);
+    verifyZeroInteractions(mockPostExecutionThread);
+    verifyZeroInteractions(mockThreadExecutor);
+  }
+
+  @Test public void testShouldFailWhenNoOrEmptyParameters() {
+    expectedException.expect(NullPointerException.class);
+    getMenus.buildUseCaseObservable(null);
+  }
+}
