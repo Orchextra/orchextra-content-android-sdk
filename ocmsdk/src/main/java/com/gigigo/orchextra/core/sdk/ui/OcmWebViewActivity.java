aab64e9e23db7d8ca5f96a2ac0dc0baa629d97db
Jose Benito <jose.benito@gigigo.com>
2017-07-05 11:15:16 +0200

    WH-305 Open webview when Chrome is not installed

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/ui/OcmWebViewActivity.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/ui/OcmWebViewActivity.java
@@ -1,7 +1,9 @@
 package com.gigigo.orchextra.core.sdk.ui;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -25,22 +27,27 @@ public class OcmWebViewActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_ocm_webview_layout);
 
+    setStatusbar();
     setToolbar();
     setWebViewFragment();
   }
 
   private void setToolbar() {
-    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-
     DetailToolbarView ocmToolbar = (DetailToolbarView) findViewById(R.id.ocmToolbar);
     ocmToolbar.setShareButtonVisible(false);
     ocmToolbar.switchBetweenButtonAndToolbar(true, false);
     ocmToolbar.blockSwipeEvents(true);
-    ocmToolbar.setOnClickBackButtonListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        finish();
-      }
-    });
+    ocmToolbar.setOnClickBackButtonListener(v -> finish());
+  }
+
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP) private void setStatusbar() {
+    if (AndroidSdkVersion.hasLollipop21()) {
+      getWindow().setStatusBarColor(getResources().getColor(R.color.oc_status_bar_color));
+    }
+
+    if (AndroidSdkVersion.hasJellyBean16()) {
+      getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
   }
 
   private void setWebViewFragment() {
