116eb998ee857483b07da715e9aba7b1e2ac3c2f
Francisco Hernández <francisco.hernandez.87@gmail.com>
2017-07-07 12:50:41 +0200

    Update number of cores for Dalvik

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxExecutor/JobExecutor.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxExecutor/JobExecutor.java
@@ -1,6 +1,8 @@
 package com.gigigo.orchextra.core.data.rxExecutor;
 
+import android.os.Build;
 import com.gigigo.orchextra.core.domain.rxExecutor.ThreadExecutor;
+import com.gigigo.orchextra.ocmsdk.BuildConfig;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
@@ -11,9 +13,9 @@ import orchextra.javax.inject.Singleton;
 
 @Singleton public class JobExecutor implements ThreadExecutor {
 
-  private static final int INITIAL_POOL_SIZE = 2;
-  private static final int MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1
-      : Runtime.getRuntime().availableProcessors() - 1;
+  private static int INITIAL_POOL_SIZE = 1;
+  private static int MAX_POOL_SIZE = 1;
+  //Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1 : Runtime.getRuntime().availableProcessors() - 1;
 
   // Sets the amount of time an idle thread waits before terminating
   private static final int KEEP_ALIVE_TIME = 10;
@@ -28,11 +30,26 @@ import orchextra.javax.inject.Singleton;
   private final ThreadFactory threadFactory;
 
   @Inject public JobExecutor() {
+    initCores();
     this.workQueue = new LinkedBlockingQueue<>();
     this.threadFactory = new JobThreadFactory();
     this.threadPoolExecutor =
         new ThreadPoolExecutor(INITIAL_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
             KEEP_ALIVE_TIME_UNIT, this.workQueue, this.threadFactory);
+  }
+
+  private void initCores() {
+    // Check if we're running on Android 5.0 or higher
+    if (Build.VERSION.SDK_INT >= 21) {
+      // Call some material design APIs here
+      INITIAL_POOL_SIZE = 2;
+      MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1
+          : Runtime.getRuntime().availableProcessors() - 1;
+    } else {
+      // Implement this feature without material design
+      INITIAL_POOL_SIZE = 1;
+      MAX_POOL_SIZE = 1;
+    }
   }
 
   @Override public void execute(Runnable runnable) {
