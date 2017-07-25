e7cd04735bafdc4daa1b4eb67f5123c61107f7e9
Jose Benito <jose.benito@gigigo.com>
2017-07-25 10:16:00 +0200

    IlllegalArgumentException

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxExecutor/JobExecutor.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/rxExecutor/JobExecutor.java
@@ -1,8 +1,6 @@
 package com.gigigo.orchextra.core.data.rxExecutor;
 
-import android.os.Build;
 import com.gigigo.orchextra.core.domain.rxExecutor.ThreadExecutor;
-import com.gigigo.orchextra.ocmsdk.BuildConfig;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
@@ -13,16 +11,13 @@ import orchextra.javax.inject.Singleton;
 
 @Singleton public class JobExecutor implements ThreadExecutor {
 
-  private static int INITIAL_POOL_SIZE = 1;
-  private static int MAX_POOL_SIZE = 1;
-  //Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1 : Runtime.getRuntime().availableProcessors() - 1;
-
   // Sets the amount of time an idle thread waits before terminating
   private static final int KEEP_ALIVE_TIME = 10;
-
   // Sets the Time Unit to seconds
   private static final TimeUnit KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS;
-
+  //Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1 : Runtime.getRuntime().availableProcessors() - 1;
+  private static int INITIAL_POOL_SIZE = 1;
+  private static int MAX_POOL_SIZE = 1;
   private final BlockingQueue<Runnable> workQueue;
 
   private final ThreadPoolExecutor threadPoolExecutor;
@@ -51,11 +46,13 @@ import orchextra.javax.inject.Singleton;
     //  MAX_POOL_SIZE = 1;
     //}
 
-
-
     INITIAL_POOL_SIZE = 2;
-    MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1
-        : Runtime.getRuntime().availableProcessors() - 1;
+    try {
+      MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors() - 1 <= 0 ? 1
+          : Runtime.getRuntime().availableProcessors() - 1;
+    } catch (IllegalArgumentException exception) {
+      MAX_POOL_SIZE = 1;
+    }
   }
 
   @Override public void execute(Runnable runnable) {
