cb8222d61cb7a751e5d1b505558b80d5a8a1938d
Jose Benito <jose.benito@gigigo.com>
2017-10-23 12:49:51 +0200

    Adding more clip to padding enum values

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/model/grid/dto/ClipToPadding.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/sdk/model/grid/dto/ClipToPadding.java
@@ -1,10 +1,15 @@
 package com.gigigo.orchextra.core.sdk.model.grid.dto;
 
 public enum ClipToPadding {
-  PADDING_NONE(0),
-  PADDING_SMALL(12),
-  PADDING_MEDIUM(6),
-  PADDING_BIG(2);
+  PADDING_16(16),
+  PADDING_14(14),
+  PADDING_12(12),
+  PADDING_10(10),
+  PADDING_8(8),
+  PADDING_6(6),
+  PADDING_4(4),
+  PADDING_2(2),
+  PADDING_NONE(0);
 
   private final int padding;
 
