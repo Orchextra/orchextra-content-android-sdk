37f8af50aebcde4568a5a05e47b31de7b0846ab7
Claudia Brito <claudia.brito@gigigo.com>
2017-11-06 09:59:21 +0100

    Create new method for logged actions

--- ocmsdk/src/main/java/com/gigigo/orchextra/ocm/callbacks/OnRequiredLoginCallback.java
+++ ocmsdk/src/main/java/com/gigigo/orchextra/ocm/callbacks/OnRequiredLoginCallback.java
@@ -1,5 +1,11 @@
 package com.gigigo.orchextra.ocm.callbacks;
 
+import android.widget.ImageView;
+import com.gigigo.orchextra.core.domain.entities.elementcache.ElementCache;
+import java.lang.ref.WeakReference;
+
 public interface OnRequiredLoginCallback {
   void doRequiredLogin();
+  void doLoggedActionRequired(ElementCache elementCache, String elementUrl, String urlImageToExpand,
+      int widthScreen, int heightScreen, WeakReference<ImageView> imageViewWeakReference);
 }
