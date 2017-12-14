3973821025a7a01707e5a1c110e3e645d6046b8a
Jose Benito <jose.benito@gigigo.com>
2017-12-14 13:13:09 +0100

    Fix: Added credentials in variants.gradle file

--- app/src/main/java/com/gigigo/sample/App.java
+++ app/src/main/java/com/gigigo/sample/App.java
@@ -16,44 +16,9 @@ import com.squareup.leakcanary.LeakCanary;
 
 public class App extends MultiDexApplication {
 
-  //public static String API_KEY = "a2966ba69f4ead1a4f1550bfda450e9fd07e6762";   //Asv project
-  //public static String API_SECRET = "f79713d7e9b0fcd69fedfb94f471106cb85d8ca4";
-
-  //WOAH RELEASE
-  //public static String API_KEY = "8286702045adf5a3ad816f70ecb80e4c91fbb8de";
-  //public static String API_SECRET = "eab37080130215ced60eb9d5ff729049749ec205";
-
-  //WOAH Debug
-  //public static String API_KEY = "8286702045adf5a3ad816f70ecb80e4c91fbb8de";
-  //public static String API_SECRET = "eab37080130215ced60eb9d5ff729049749ec205";
-
-  //[UAT][CSE] - WOAH SITC STAGING
-  //public static String API_KEY = "adfc8ba4340828a054bf061f692707a197af96cb";
-  //public static String API_SECRET = "677cf75a17aeec144ee402c281ad3a732d736a8a";
-
-  //ANDROID SDK - STAGING
-  //public static String API_KEY = "34a4654b9804eab82aae05b2a5f949eb2a9f412c";
-  //public static String API_SECRET = "2d5bce79e3e6e9cabf6d7b040d84519197dc22f3";
-
-  //WOAH MARKETS RELEASE
-  //public static String API_KEY = "ef08c4dccb7649b9956296a863db002a68240be2";    //Woah project
-  //public static String API_SECRET = "6bc18c500546f253699f61c11a62827679178400";
-
-  //WOAH QA RELEASE
-  //public static String API_KEY = "ad46332237cbb9fd38ad38470a9bee6d4892d770";    //Woah project
-  //public static String API_SECRET = "f5e2dd3ba0de46964775fc1b48c4895c73d263c5";
-
-  //REPSOL RELEASE
-  //public static String API_KEY = "7bb9fa0f9b7a02846383fd6284d3c74b8155644c";
-  //public static String API_SECRET = "3295dc8de90300e2977e6cec5b28b614fc644934";
-
-  //REPSOL DEBUG
-  //public static String API_KEY = "53cf8785f1f02b5a69adbddee58bb33b6094eb5b";    //Woah project
-  //public static String API_SECRET = "e1d718aefde8aab04621a7acf1771dfbf5884fef";
-
-  //[UAT][INTERNAL] DEMO APP
-  //public static String API_KEY = "338d65a6572be208f25a9a5815861543adaa4abb";
-  //public static String API_SECRET = "b29dac01598f9d8e2102aef73ac816c0786843ef";
+  /**
+  / Project credentials are moved to variants.gradle file
+   */
 
   private OnRequiredLoginCallback onDoRequiredLoginCallback =  new OnRequiredLoginCallback() {
     @Override public void doRequiredLogin() {
@@ -153,7 +118,7 @@ public class App extends MultiDexApplication {
             .setEnabledStatusBar(false);
     Ocm.setStyleUi(ocmStyleUiBuilder);
 
-    Ocm.setBusinessUnit("it");
+    Ocm.setBusinessUnit(BuildConfig.BUSSINES_UNIT);
   }
 
   private void enableStrictMode() {
