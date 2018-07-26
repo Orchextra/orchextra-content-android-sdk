3d90a3cdafa820c456bc092b01aeb3ba1ff75a6e
Manuel Rebollo <mrebollob@gmail.com>
2018-07-26 11:20:06 +0200

    Add customization for content in full screen view

---
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/domain/entities/elements/ElementSectionView.kt
@@ -0,0 +1,8 @@
+package com.gigigo.orchextra.core.domain.entities.elements
+
+class ElementSectionView {
+
+  var text: String? = null
+  var imageUrl: String? = null
+  var imageThumb: String? = null
+}
