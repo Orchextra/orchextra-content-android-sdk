54ea65ed004ac2e4a06852f2184a11b55ad4a076
Manuel Rebollo <mrebollob@gmail.com>
2018-10-09 11:35:30 +0200

    Remove unused exception

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/OcmNetworkDataSource.kt
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/OcmNetworkDataSource.kt
@@ -4,7 +4,6 @@ import com.gigigo.orchextra.core.data.api.services.OcmApiService
 import com.gigigo.orchextra.core.data.mappers.toContentData
 import com.gigigo.orchextra.core.data.mappers.toMenuContentData
 import com.gigigo.orchextra.core.data.rxException.ApiMenuNotFoundException
-import com.gigigo.orchextra.core.data.rxException.ApiSectionNotFoundException
 import com.gigigo.orchextra.core.data.rxException.NetworkConnectionException
 import com.gigigo.orchextra.core.domain.entities.contentdata.ContentData
 import com.gigigo.orchextra.core.domain.entities.menus.MenuContentData
@@ -30,7 +29,7 @@ class OcmNetworkDataSource @Inject constructor(private val ocmApiService: OcmApi
     throw NetworkConnectionException()
   }
 
-  @Throws(ApiSectionNotFoundException::class, NetworkConnectionException::class)
+  @Throws(NetworkConnectionException::class)
   fun getSectionElements(section: String): ContentData {
     try {
       ocmApiService.getSectionData(section).execute().body()?.let {
@@ -39,7 +38,6 @@ class OcmNetworkDataSource @Inject constructor(private val ocmApiService: OcmApi
       }
     } catch (e: Exception) {
       Timber.e(e, "getSectionElements()")
-      throw ApiSectionNotFoundException()
     }
     throw NetworkConnectionException()
   }
